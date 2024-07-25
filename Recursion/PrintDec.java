10c98bf3108d90e64ab2a0f416c4ee816365fb46
DHAVAL-SAVANI <166829075+DHAVAL-SAVANI@users.noreply.github.com>
2024-07-25 06:47:34 +0530

    Print number from n to 1 using recursion

---
+++ Recursion/PrintDec.java
@@ -0,0 +1,13 @@
+public class PrintDec {
+    public static void main(String args[]){
+        printDec(10);
+    }
+    public static void printDec(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        System.out.print(n+" ");
+        printDec(n-1);
+    }
+}
