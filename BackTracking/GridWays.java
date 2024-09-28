3a9c65329af8911aa3f2794304db5c7d178edb90
DHAVAL-SAVANI <166829075+DHAVAL-SAVANI@users.noreply.github.com>
2024-09-28 21:23:59 +0530

    Grid Way problem added

---
+++ BackTracking/GridWays.java
@@ -0,0 +1,23 @@
+public class GridWays {
+    static int ways = 0;
+    public static int gridWays(int i, int j, int n, int m){
+        //base case
+        if(i==n-1 && j==m-1){
+            return 1;
+        }
+        else if(i==n || j==m){
+            return 0;
+        }
+        
+        //recursion
+        int w1 = gridWays(i+1,j,n,m); // down
+        int w2 = gridWays(i,j+1,n,m); // right
+        return w1+w2;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 4;
+        System.out.println(gridWays(0,0,n,m));
+    }
+}
